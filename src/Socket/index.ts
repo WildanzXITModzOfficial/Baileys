eb17c497ef0ef60b92b0241b150275be4d7baf04
WildanzXITModz Official <cobrajawatzy771@gmail.com>
2026-06-10 23:15:13 +0700

    Wildanz Log Baileys

--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -2,6 +2,16 @@ import { DEFAULT_CONNECTION_CONFIG } from '../Defaults'
 import type { UserFacingSocketConfig } from '../Types'
 import { makeCommunitiesSocket } from './communities'
 
+// ✅ TAMBAH LOG KAMU DI SINI
+const chalk = require('chalk')
+console.log(chalk.cyan(`
+==================================================
+🤖 WILDANZ BAILEYS MOD
+👑 Powered By WildanzXITModz Official
+🔌 Status: Menghubungkan ke WhatsApp...
+==================================================
+`))
+
 // export the last socket layer
 const makeWASocket = (config: UserFacingSocketConfig) => {
 	const newConfig = {
